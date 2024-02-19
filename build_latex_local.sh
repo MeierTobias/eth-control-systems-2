55b930c7a07a0c799ba9039acd040784fe2ef4d9
Tobias Meier <meier.tobias@hotmail.com>
2024-02-19 11:41:44 +0100

    initial setup

---
+++ build_latex_local.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+COMMAND="latexmk -pdf -cd -interaction=nonstopmode -file-line-error -jobname=main_full -outdir=../build ./src/main.tex ; 
+    sed '0,/{1}/s//{0}/' -i src/var.tex ; 
+    latexmk -pdf -cd -interaction=nonstopmode -file-line-error -jobname=main -outdir=../build ./src/main.tex ;
+    sed '0,/{0}/s//{1}/' -i src/var.tex"
+exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/usr/src/app -w /usr/src/app texlive/texlive:latest sh -c "$COMMAND"
